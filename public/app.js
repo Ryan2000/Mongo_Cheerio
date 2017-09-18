9ab11ea45796446837733109da39fe260bad75cb
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-09-17 18:16:51 -0700

    no message

---
+++ public/app.js
@@ -0,0 +1,44 @@
+/**
+ * Created by ryanhoyda on 9/15/17.
+ */
+
+
+//string variable that holds html
+var articleHtml = '<div class="row"><div class="col 12"><div class="card">' +
+    '<h4 class="card-body"' +
+    'id="card=content"><div class="d-flex justify-content-between">' +
+    '<h4 class="card-title">${title}</h4><a href="#" class="btn btn-danger">Save Article</a>' +
+    '</div></br><p class="card-text">${summary}</p><a href="${link}"'+
+    'class="btn btn-primary">View Article</a></div></div></div></div>';
+
+$(document).ready(function(){
+
+    $( "#startScrape" ).click(function() {
+        //alert("scrape has begun");
+
+        $.getJSON("/scrape", function(data) {
+            $('#articles').empty();
+            // For each one
+            var count = data.count;
+            var articles = data.articles;
+
+            for (var i = 0; i < articles.length; i++) {
+                var title = articles[i].title;
+                var link = articles[i].link;
+                var summary = articles[i].summary;
+                $("#articles").append(articleHtml.replace("${title}", title)
+                    .replace("${summary}", summary).replace("${link}", link));
+            }
+            alert('Articles Found ' + count);
+        });
+
+
+
+    });
+
+
+
+
+});
+
+
--- server.js
+++ server.js
@@ -6,15 +6,18 @@
  * Created by ryanhoyda on 9/3/17.
  */
 // Dependencies
+var fs = require('fs');
 var express = require("express");
 var mongojs = require("mongojs");
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
 var mongoose = require('mongoose');
+mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
+app.use(express.static("public"));
 
 // Database configuration
 var databaseUrl = "mongodb://localhost:27017/news_sites";
@@ -23,6 +26,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
+//db schema ----------------------------------
 db.once('open', function () {
     var wsjSchema = mongoose.Schema({
         title: String,
@@ -30,14 +34,20 @@ db.once('open', function () {
         summary: String
     });
 
-    var WjsModel = mongoose.model('WSJ-Model', wsjSchema);
+    var WsjModel = mongoose.model('WSJ-Model', wsjSchema);
+
+//------------------------------------------
+
 
     // Main route (simple Hello World Message)
     app.get("/", function (req, res) {
-        res.send("Hello world");
+        res.redirect('index.html');
     });
 
     // Retrieve data from the db
+    //processing http get request
+    //function name all - endpoint, function
+    //and then retrieving all info from database
     app.get("/all", function (req, res) {
         // Find all results from the scrapedData collection in the db
         db.scrapedData.find({}, function (error, found) {
@@ -52,64 +62,106 @@ db.once('open', function () {
         });
     });
 
-// Scrape data from one site and place it into the mongodb db
+    // Scrape data from one site and place it into the mongodb db
+    //scrape is endpoint, function
+    //processes on this endpoint when it receives a get request.
     app.get("/scrape", function (req, res) {
         // Make a request for the news section of WSJ
         request("https://www.wsj.com/", function (error, response, html) {
-            // Load the html body from request into cheerio
-            var $ = cheerio.load(html);
-            // For each element with a "title" class
-            $("h3.wsj-headline").each(function (i, element) {
-                // Save the text and href of each link enclosed in the current element
-                var title = $(element).children().text();
-                var summary = $(element)
-                    .siblings('.wsj-card-body')
-                    .children('.wsj-summary')
-                    .children('span')
-                    .text();
-                var link = $(element).children().attr("href");
+            //json has 2 props count and articles
+            var json = {
+                count: 0,
+                articles: []
+            };
+            //The web scraping is an asyn process so we need
+            //to wrap it in a Promise
+            var promise = new Promise(function(resolve, reject){
 
-                //Fall back if the first approach did not grab a summary
-                if (summary === '') {
-                    summary = $(element)
-                        .siblings('.wsj-summary')
+                //All new articles are stored here
+                var articles = [];
+
+                //This will contain query promises to the db
+                //for each article. That way they are still unique
+                var findPromises = [];
+
+                // Load the html body from request into cheerio
+                var $ = cheerio.load(html);
+                // For each element with a "title" class
+                $("h3.wsj-headline").each(function (i, element) {
+                    // Save the text and href of each link enclosed in the current element
+                    var title = $(element).children().text();
+                    var summary = $(element)
+                        .siblings('.wsj-card-body')
+                        .children('.wsj-summary')
                         .children('span')
                         .text();
-                }
+                    var link = $(element).children().attr("href");
 
-                // console.log(title);
-                // console.log(link);
-                // console.log(summary);
+                    //Fall back if the first approach did not grab a summary
+                    if (summary === '') {
+                        summary = $(element)
+                            .siblings('.wsj-summary')
+                            .children('span')
+                            .text();
+                    }
 
-                // If this found element had both a title and a link
-                if (title && link && summary) {
-                    var article = new WsjModel({
-                        title: title,
-                        link: link,
-                        summary: summary
-                    });
-                    WsjModel.find({
-                        title: title,
-                        link: link,
-                        summary: summary
-                    }, function(resultSet){
-                        //TODO: Test result set is empty
-
-                        //if empty save
-                        article.save(function(error, article){
-                            if(error){
-                                console.error(error);
-                            } else {
-                                console.log('Saved ' + article);
-                            }
+                    // If this found element had both a title and a link
+                    if (title && link && summary) {
+                        var article = new WsjModel({
+                            title: title,
+                            link: link,
+                            summary: summary
                         });
+
+                        //Create a new promise for each article
+                        //The query to the db is async so each query
+                        //has to be wrapped in a Promise
+                        var findPromise = new Promise(function(resolve, reject){
+                            //Perform a db query
+                            WsjModel.find({
+                                'title': title,
+                                'link': link,
+                                'summary': summary
+                            }).exec(function(err, results){
+                                var count = results.length;
+                                if(count === 0){
+                                    //This is a new article so pass it to
+                                    //the resolve function
+                                    resolve(article);
+                                } else {
+                                    //This article exists so just pass
+                                    //undefined to the resolve function
+                                    resolve(undefined);
+                                }
+                            });
+                        });
+                        //Now execute all db queries
+                        findPromises.push(findPromise);
+                    }
+                });
+
+
+                Promise.all(findPromises).then(function(results){
+                    var savePromises = [];
+                    results.forEach(function(elem){
+                        if(elem){
+                            savePromises.push(elem.save());
+                        }
                     });
-                }
+                    Promise.all(savePromises).then(function(models){
+                        models.forEach(function(elem){
+                            articles.push(elem);
+                        });
+                        resolve(articles);
+                    });
+                });
+            });
+            promise.then(function(articles){
+                json.count = articles.length;
+                json.articles = articles;
+                res.json(json);
             });
         });
-
-        // Send a "Scrape Complete" message to the browser
-        res.send("Scrape Complete");
     });
 
 
